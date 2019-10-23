186380566b69e511d9f9e7b678a9888737446f11
Vinicius Oliveira da SIlva <viniciusoliveiras12@hotmail.com.br>
2019-10-23 09:53:50 -0300

    enum

--- Projeto Itp2019.2/imageprocessors.h
+++ Projeto Itp2019.2/imageprocessors.h
@@ -1,6 +1,9 @@
 #ifndef IMAGEPROCESSORS_H
 #define IMAGEPROCESSORS_H
 #include <stdio.h>
+typedef enum{
+    false, true
+}Bool;
 typedef struct {
     unsigned char r;
     unsigned char g;
@@ -9,6 +12,7 @@ typedef struct {
 typedef struct{
     int line;
     int col;
+    Bool state;
     Pixel **img;
 }Image;
 #endif
