e3562172abe7469775983b6ae8db8388bd6866a1
Vinicius Oliveira da SIlva <viniciusoliveiras12@hotmail.com.br>
2019-10-30 21:32:10 -0300

    tentando poligono

--- Projeto Itp2019.2/imageprocessors.c
+++ Projeto Itp2019.2/imageprocessors.c
@@ -13,55 +13,30 @@ void limpar(Image *img, int r, int g, int b){//Limpa a imagem e bota uma cor esp
     }
 }
 void line(Image *img, int x0, int y0,int xf,int yf,int r,int g,int b){
-    int deltax,deltay,quadrantex,quadrantey,erro,erro2;
-    deltax =  abs(xf - x0);
-    deltay =  abs(yf - x0);
-    if(x0 < xf){
-        quadrantex = 1;
-    }
-    else{
-        quadrantex = -1;
-    }
-    if(y0 < yf){
-        quadrantey = 1;
-    }
-    else{
-        quadrantey = -1;
-    }
-    if(quadrantex > quadrantey){
-        erro = quadrantex/2;
-    }
-    else{
-        erro = -quadrantey/2;
-    }
-    while (x0 != xf && y0 != yf){
-        img->img[x0][y0].r = r ;
-        img->img[x0][y0].g = g ;
-        img->img[x0][y0].b = b ;
-        
-        erro2 = erro;
-        if(erro2 > -deltax){
-            erro -= deltay;
-            x0 += quadrantex;
+    int dx = abs(x0-xf), sx = x0<xf ? 1 : -1;
+    int dy = abs(y0-yf), sy = y0<yf ? 1 : -1;
+    int err = (dx>dy ? dx : -dy)/2, e2;
+    while(1){
+        img->img[x0][y0].r = r;
+        img->img[x0][y0].g = g;
+        img->img[x0][y0].b = b;
+        if (x0==xf && y0==yf){ 
+            break;
         }
-        if(erro2 < deltay){
-            erro += deltax;
-            y0 += deltay;  
-        }
+        e2 = err;
+        if (e2 >-dx) { err -= dy; x0 += sx; }
+        if (e2 < dy) { err += dx; y0 += sy; }
     }
 }
-void fill(Image *img, int r,int g, int b){
-    if (r == 0 && g == 0 && b == 0){      
-    }
-}
+ 
+// void fill(Image *img, int r,int g, int b){
+//     if (r == 0 && g == 0 && b == 0){      
+//     }
+// }
 void polygono(Image *img, int r, int g, int b,FILE *entrada){
-    int pontos,j,i;
+    int pontos,j,i,x0,y0,xf,yf;
+    int x1,y1;
     fscanf(entrada, "%i", &pontos);
-    Ponto arpo[pontos];
-    for (i = 0; i < pontos; i++){
-        fscanf(entrada, "%i %i", &arpo[i].x0, &arpo[i].y0);
-    }
-    for(j = 0; j < pontos; j++){    
-        return;
-    }
+    fscanf(entrada, "%i %i %i %i", &x0,&y0,&xf,&yf);
+    line(img,x0,y0,xf,yf,r,g,b);
 }
