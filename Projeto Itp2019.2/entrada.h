8a934c26ac82420c345dc2336b934fa4ffae77c8
Vinicius Oliveira da SIlva <viniciusoliveiras12@hotmail.com.br>
2019-10-31 22:55:18 -0300

    poligonos

--- Projeto Itp2019.2/entrada.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENTRADA_H
-#define ENTRADA_H
-#include <stdio.h>
-#include "imageprocessors.h"
-void startimage(Image *img); //Função de inicializar imagem
-Bool escolherfuncao(char nomedafuncao, FILE *fp);//Em processo de criação
-#endif 
